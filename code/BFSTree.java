cf9af084c4894bb1eb94e9f1d2d648436bd5af03
sunilcotterill <sunilcotterill@sunils-air.lan>
2020-12-07 16:15:04 -0500

    started BFSTree

--- code/BFSTree.java
+++ code/BFSTree.java
@@ -1,8 +1,49 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class BFSTree {
-    // TODO: consider what fields you might want
-    // Hint: probably not (just) a graph and source vertex
-    
-    // TODO: implement constructor
-    
-    // TODO: implement getDistanceTo method
+	private IntGraph graph;
+	private int source;
+	private Map<Integer, Integer> distances = new HashMap<>(); // vertices to
+																// integers
+	private Map<Integer, Integer> parents = new HashMap<>();
+	// TODO: consider what fields you might want
+	// Hint: probably not (just) a graph and source vertex
+
+	// TODO: implement constructor
+	BFSTree(IntGraph graph, int source) {
+		this.graph = graph;
+		this.source = source;
+		BFS();
+	}
+
+	private void BFS() {
+		
+		for (int node : graph.getVertices()) {
+			// for every vertex
+			distances.put(node, -1);
+			// initialize distance and parent
+			parents.put(node, null);
+		}
+		
+		Queue<Integer> q = new LinkedList<>();
+		this.distances.put(source, 0);
+		q.add(source);
+		while (!q.isEmpty()) {
+			int u = q.remove();
+			for (int v : adjacencyList.get(u)) {
+
+				if (distances.get(v) == -1) {
+
+					distances.put(v, distances.get(u) + 1);
+					parents.put(v, u);
+					q.add(v);
+				}
+			}
+		}
+
+	}
+// TODO: implement getDistanceTo method
 }
